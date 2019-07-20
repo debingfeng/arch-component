33f254e4690da1eb81fbb0a31841b5c1330b0c11
“yo“fdb <fengdebing@oaserver.dw.gdbbk.com>
2019-07-20 18:00:51 +0800

    增加SCSS

--- loading/loading.js
+++ loading/loading.js
@@ -1,6 +1,72 @@
 var $_loading = (function () {
     var doc = document;
     /**
+     * Create element
+     */
+    var createElement = function (tag) {
+        return document.createElement(tag);
+    };
+    /**
+     * Has class
+     * @param elements
+     * @param cName
+     * @returns {boolean}
+     */
+    var hasClass = (elements, cName) => {
+        return !!elements.className.match(new RegExp("(\\s|^)" + cName + "(\\s|$)"));
+    };
+    /**
+     * Add Class
+     * @param elements
+     * @param cName
+     */
+    var addClass = (elements, cName) => {
+        if (!hasClass(elements, cName)) {
+            elements.className += " " + cName;
+        }
+    };
+    /**
+     * remove Class
+     * @param elements
+     * @param cName
+     */
+    var removeClass = (elements, cName) => {
+        if (hasClass(elements, cName)) {
+            elements.className = elements.className.replace(new RegExp("(\\s|^)" + cName + "(\\s|$)"), " ");
+        }
+    };
+
+    /**
+     * Block page scrolling
+     */
+    function stopScroll() {
+        // 防止页面股东
+        var docBody = document.body;
+
+        addClass(document.firstElementChild, 'wh-full');
+        addClass(docBody, 'wh-full');
+        docBody.style.overflow = 'hidden';
+        docBody.style.position = 'relative';
+        document.addEventListener('touchmove', function (event) {
+            event.preventDefault();
+            return false;
+        });
+    }
+
+    /**
+     * Unblock page scrolling
+     */
+    function recoverScroll() {
+        var docBody = document.body;
+        removeClass(document.firstElementChild, 'wh-full');
+        removeClass(docBody, 'wh-full');
+        docBody.style.overflow = 'visible';
+        docBody.style.position = 'static';
+        document.body.removeEventListener('touchmove', function () {
+        });
+    }
+
+    /**
      * 创建loadingHTML元素
      */
     var createLoader = function () {
@@ -10,9 +76,9 @@ var $_loading = (function () {
             loader = createElement("div"),
             loadContent = createElement("p");
 
-        addClass(wrapper,'over-layer xtc-loading');
+        addClass(wrapper, 'over-layer xtc-loading');
 
-        addClass(loadContainer,'xtc-load-container');
+        addClass(loadContainer, 'xtc-load-container');
 
         addClass(loader, "xtc-loader");
         addClass(loadContent, "load-content");
